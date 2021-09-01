7742f72b557db8bd5a14a986cbdedce2310e2e90
GianmarcoPerini <g.marco.perini@gmail.com>
2021-09-01 17:18:05 +0200

    scelta difficoltà

--- main.js
+++ main.js
@@ -2,29 +2,82 @@ let listaBomba = [];
 let numeriBomba = generatoreNumeriUnici(1, 100, 16, listaBomba);
 console.log(listaBomba);
 let listaSalvi= [];
+let capitalizzazione = prompt("Scegli il livello di difficoltà: Facile, Medio, Difficile")
+let difficolta = capitalizzazione.charAt(0).toUpperCase() + capitalizzazione.slice(1);
+let y = level(difficolta)
 
-for(i=0;i<3;i++){
+if(y == undefined){
+    alert("Scelta non valida")
+    y = 4
+}
+
+
+
+while(listaSalvi.length < y){
     let numeroPrompt = parseInt(prompt("Iserisci un numero"));
-    listaSalvi.push(numeroPrompt);
-
-    if(listaBomba.includes(numeroPrompt)){
-        console.log("morto");
-        break
+    if(listaSalvi.indexOf(numeroPrompt) == -1) {
+        listaSalvi.push(numeroPrompt);
+        console.log(listaSalvi);
     }else{
-        console.log("vivo");
+            alert("Hai gia inserito questo numero")
+            numeroPrompt
     }
 
 
+    if(listaBomba.includes(numeroPrompt)){
+        console.log("Sei morto, hai fatto " + listaSalvi.length + " passi prima di morire");
+        break
+    }else if(listaSalvi.length == y){
+        console.log("COMPLIMENTI sei unscito indenne dal CAMPO MINATO");
+    }
+    else{
+        console.log("vivo");
+    }
 }
 
 
 
 
 
+// for(i=0;listaSalvi.length < 16;i++){
+//     let numeroPrompt = parseInt(prompt("Iserisci un numero"));
+//     if(listaSalvi.indexOf(numeroPrompt) == -1) {
+//         listaSalvi.push(numeroPrompt);
+//         console.log(listaSalvi);
+//     }else{
+//             alert("Hai gia inserito questo numero")
+//             numeroPrompt
+//     }
+
+
+//     if(listaBomba.includes(numeroPrompt)){
+//         console.log("Sei morto, hai fatto " + listaSalvi.length + " passi prima di morire");
+//         break
+//     }else if(listaSalvi.length == 16){
+//         console.log("COMPLIMENTI sei unscito indenne dal CAMPO MINATO");
+//     }
+//     else{
+//         console.log("vivo");
+//     }
+// }
 
 
 
 
+
+
+
+
+
+function level(livello){
+    if(livello == "Facile"){
+        return 4
+    } else if (livello == "Medio"){
+        return 10
+    } else if( livello == "Difficile"){
+        return 16
+    }
+}
 
 function generatoreNumeriUnici(min, max, lunghezzaArray, listaDestinazione){
     while(listaDestinazione.length < lunghezzaArray){
