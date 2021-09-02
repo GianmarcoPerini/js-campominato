60fe1d0b5cd8d30d3526a199c08c40d025ef75f6
GianmarcoPerini <g.marco.perini@gmail.com>
2021-09-02 12:58:34 +0200

    rev_1

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
 let listaBomba = [];
 let numeriBomba = generatoreNumeriUnici(1, 100, 16, listaBomba);
-console.log(listaBomba);
 let listaSalvi= [];
-let capitalizzazione = prompt("Scegli il livello di difficoltà: Facile, Medio, Difficile")
+let capitalizzazione = prompt("Scegli il livello di difficoltà: Facile, Medio, Difficile");
 let difficolta = capitalizzazione.charAt(0).toUpperCase() + capitalizzazione.slice(1);
-let y = level(difficolta)
+let listaLivelli = ["Facile","Medio","Difficile"];
+let y = level(difficolta);
 
-if(y == undefined){
-    alert("Scelta non valida")
-    y = 4
+// SCELTA DELLA DIFFICOLTÀ
+while(!listaLivelli.includes(difficolta)){                                                                  
+    alert("Scelta non valida");
+    capitalizzazione = prompt("Scegli il livello di difficoltà: Facile, Medio, Difficile");
 }
-
-while(listaSalvi.length < y){
+// INSERIMENTO NUMERI
+while(listaSalvi.length < y){                                                                               
     let numeroPrompt = parseInt(prompt("Inserisci un numero tra 1 e 100"));
-    if(numeroPrompt > 100 || numeroPrompt <= 0){
-        alert("Numero inserito non valido")
-        numeroPrompt
-    }else if(listaSalvi.indexOf(numeroPrompt) == -1) {
+
+    if(numeroPrompt > 100 || numeroPrompt <= 0 || isNaN(numeroPrompt)){
+        alert("Numero inserito non valido");
+        numeroPrompt;
+    }else if(listaSalvi.indexOf(numeroPrompt) == -1) {    // CONTROLLO SE NUMERO INSERITO È DOPPIO                                                  
         listaSalvi.push(numeroPrompt);
         console.log(listaSalvi);
     }else{
-        alert("Hai gia inserito questo numero")
-        numeroPrompt
+        alert("Hai gia inserito questo numero");
+        numeroPrompt;
     }
-    if(listaBomba.includes(numeroPrompt) && listaSalvi.length <= 1){
+
+
+     // RISULTATO
+    if(listaBomba.includes(numeroPrompt) && listaSalvi.length <= 1){                                       
         console.log("Sei morto, hai fatto " + listaSalvi.length + " passo prima di ESPLODERE");
         break
     }else if(listaBomba.includes(numeroPrompt)){
@@ -39,24 +44,20 @@ while(listaSalvi.length < y){
 
 
 
-
-
-
-
-
 function level(livello){
-    if(livello == "Facile"){
-        return 4
-    } else if (livello == "Medio"){
-        return 10
-    } else if( livello == "Difficile"){
-        return 16
+    switch(livello){
+        case "Facile":
+            return  4;
+        case "Medio":
+            return 10;
+        case "Difficile":
+            return 16;
     }
 }
 
 function generatoreNumeriUnici(min, max, lunghezzaArray, listaDestinazione){
     while(listaDestinazione.length < lunghezzaArray){
-        let x = Math.floor(Math.random() * (max - min + 1) + min)
+        let x = Math.floor(Math.random() * (max - min + 1) + min);
         if(listaDestinazione.indexOf(x) == -1) {
             listaDestinazione.push(x);
         }
